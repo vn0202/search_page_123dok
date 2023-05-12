55e62adfc1a18a1fc665e48dd834de0dc68ade37
vn0202 <nghiavuxp0202@gmail.com>
2023-05-12 09:23:41 +0700

    desktop page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -11,12 +13,26 @@ module.exports = {
 
 
       },
+      border:{
+        '1.5':'1.5px',
+      },
       boxShadow: {
         '3xl': '0px 5px 5px -5px rgba(0, 0, 0, 0.25)',
       },
       borderRadius:{
         '4xl':'30px',
       },
+      fontSize:{
+        '3.25xl': ['32px', {
+          lineHeight: '48px',
+        }],
+      },
+      fontFamily: {
+        libre: ['"Be Vietnam Pro"', ...defaultTheme.fontFamily.sans]
+      },
+      margin:{
+        '5.5':'22px',
+      }
 
     },
   },
